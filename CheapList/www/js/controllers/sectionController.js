30ee9b5cd97ef2a6e50925de3a7e56a51b3f1a80
zham1 <frantziraci@gmail.com>
2017-01-08 09:27:05 +0100

    mise a jour
    ajout de data dans section provider pour les test en attendant le serveur, 
    modification de la route section pour sectionList,
    ajout img_url dans le model et sur section.html pour afficher les image des rayons dans les templates

--- CheapList/www/js/controllers/sectionController.js
+++ CheapList/www/js/controllers/sectionController.js
@@ -6,34 +6,12 @@ app.controller('sectionList',function($scope,sectionProvider){
 
 			$scope.sections = sectionProvider.getSections();
 
-	/*$scope.sections = 
-	[{
-			"name": "produit frais",
-			"category":{
-				"fromage": "fromage",
-				"yahourt": "yahourt",
-				"creme fraiche": "toto@creme fraiche.com"
-				},
-			"description" : " Rayon produit frais",
-			"id" : "1",
-			"pict": "http://placehold.it/140x100"
-		},
-		{
-			"name": "conserve",
-			"category":{
-				"boite-f": "boite en fer",
-				"boite-v": "boite en verre",
-				"boite-b": "Boite bio"
-				},
-			"description" : " Rayon boites de conserve",
-			"id" : "2",
-			"pict": "http://placehold.it/140x100"
-	}]*/
+	
 })
 
 app.controller('SetListName', function(){
 	
 })
 app.controller('SetList',function(){
-	
+
 })
