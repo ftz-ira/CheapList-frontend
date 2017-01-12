faaae93a43e30128911630aa176195f11d5e7e07
zham1 <frantziraci@gmail.com>
2017-01-12 08:20:55 +0100

    préparation du put produit dans list à finir

--- CheapList/www/js/controllers/listControler.js
+++ CheapList/www/js/controllers/listControler.js
@@ -2,9 +2,28 @@
 
 app.controller('list',function($http){
 
-	var productList[];
-
 	$scope.addToList = function(product_quantity,productId,){
 
+		var url = 'http://localhost:8080/cheaplist/lists/22/element/';
+
+		var el = {
+			idProduct : productId,
+			productQuantity : product_quantity
+		}
+		
+		$http.put(url).success(
+			function (response){
+
+			if(response){
+				//sections = response;
+				console.log(response);
+				$scope.categories = response;
+		
+				console.log("ok");
+				
+			}else{
+				console.log("fail");
+			}
+		})
 	};
 })
