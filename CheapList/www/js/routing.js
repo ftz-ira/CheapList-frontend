e410560ef37ac6e8f3bbd90a51f5b384836f3967
zham1 <frantziraci@gmail.com>
2017-01-15 15:18:49 +0100

    up to merge with develop local

--- CheapList/www/js/routing.js
+++ CheapList/www/js/routing.js
@@ -7,47 +7,54 @@ app.config(function ($stateProvider,  $locationProvider){
 	$locationProvider.html5Mode(true).hashPrefix('!');
 
 	$stateProvider.state('homepage',{
-	 	cache: cacheActive,
-	 	url: '/',
-	 	templateUrl: 'js/templates/homepage.html',
-	 	controller: 'homepage'
-	 })
+		cache: cacheActive,
+		url: '/',
+		templateUrl: 'js/templates/homepage.html',
+		controller: 'homepage'
+	})
 
 	$stateProvider.state('section',{
-	 	cache: cacheActive,
-	 	url: '/section/:listId',
-	 	templateUrl: 'js/templates/section.html',
-	 	controller: 'sectionList',
-	 })
+		cache: cacheActive,
+		url: '/section/:listId',
+		templateUrl: 'js/templates/section.html',
+		controller: 'sectionList',
+	})
 	$stateProvider.state('category',{
-	 	cache: cacheActive,
-	 	url: '/category/:sectionId',
-	 	templateUrl: 'js/templates/category.html',
-	 	controller: 'categories'
-	 })
+		cache: cacheActive,
+		url: '/category/:sectionId',
+		templateUrl: 'js/templates/category.html',
+		controller: 'categories'
+	})
 	$stateProvider.state('product',{
-	 	cache: cacheActive,
-	 	url: '/product/:categoryId',
-	 	templateUrl: 'js/templates/product.html',
-	 	controller: 'getProduct'
-	 })
+		cache: cacheActive,
+		url: '/product/:categoryId',
+		templateUrl: 'js/templates/product.html',
+		controller: 'getProduct'
+	})
+
+	// $stateProvider.state('scanner',{
+	// 	cache: cacheActive,
+	// 	url: '/scanner',
+	// 	templateUrl: 'js/templates/scanner.html',
+	// 	controller: 'scannerController'
+	// })
+
+	$stateProvider.state('checkshop',{
+		cache: cacheActive,
+		url: '/checkshop',
+		templateUrl: 'js/templates/checkshop.html'
+
+	})
+
+	$stateProvider.state('shopgrid',{
+		cache: cacheActive,
+		url: '/shopgrid',
+		templateUrl: 'js/templates/shopgrid.html'
+	})
 
 	$stateProvider.state('list',{
 		cache: cacheActive,
 		url: '/list/',
 		controller: 'listController'
 	})
-
-	$stateProvider.state('checkshop',{
-	 	cache: cacheActive,
-	 	url: '/checkshop',
-	 	templateUrl: 'js/templates/checkshop.html'
-	 	
-	 })
-
-	$stateProvider.state('shopgrid',{
-	 	cache: cacheActive,
-	 	url: '/shopgrid',
-	 	templateUrl: 'js/templates/shopgrid.html'
-	 })
 });
