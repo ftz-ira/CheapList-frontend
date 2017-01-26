f2bb2cd7b79e93d9f2300691052b90d47f6a5f0b
zham1 <frantziraci@gmail.com>
2017-01-26 07:51:03 +0100

    ajout du footer
    modification du gitignore commentaire ajouter pour platforms

--- cheaplist/platforms/ios/www/js/directives/bottomNav.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-app.directive('bottomNav',function(){
-	
-	return {
-		template: '<ion-footer-bar class="bar-assertive"><a  ng-href="#/app/choicemode"><h1 class="title">J\'ai fini ma liste</h1></a></ion-footer-bar>'
-	}
-})
